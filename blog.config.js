4ca72b79178eebbeb219a04eda8d327cb7b293dd
xiaoyefly <xiaoyefly666@gmail.com>
2024-03-26 23:40:14 +0800

    修改首页地址

--- blog.config.js
+++ blog.config.js
@@ -27,7 +27,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '小叶', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '莫听穿林打叶声， 何妨吟啸且徐行', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-ten-beige.vercel.app/', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://awesome-nextjs-notion-blog-pi.vercel.app/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'ai,game,阅读,旅行,游戏,思考', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'xiaoyefly666@gmail.com', // 邮箱地址 例如mail@tangly1024.com
